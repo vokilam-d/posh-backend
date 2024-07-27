59670a84a7cf21911a511f1985b073b215fe72fe
Denys Malikov <ryki.admirala@gmail.com>
2024-07-27 17:05:55 +0300

    Add "Health" for online/offline status on UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { config } from './config';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductOptionModule } from './modules/product-option/product-option.module';
 import { ProductModule } from './modules/product/product.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ProductModule } from './modules/product/product.module';
     CategoryModule,
     ProductOptionModule,
     ProductModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
